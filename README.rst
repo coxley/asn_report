059867be8ff62abeabecfe6290b6ab08e47f880a
coxley <codey@cwv.net>
2015-04-06 00:26:14 -0400

    Feature added: Netflow v5 collector vs. pcap

--- README.rst
+++ README.rst
@@ -9,15 +9,14 @@ asn_report
 Description
 -----------
 
-``asn_report`` is a small Flask application that captures live traffic destined
-to the internet and displays a chart of to get an idea of which ASes you send
-more to.
+``asn_report`` is a small Flask application that parses traffic destined to the 
+internet and displays a chart of to get an idea of which ASes you send more to.
 
 It's composed of two things: 
 
-    1. Python utility that uses scapy to capture IP traffic (or [X] traffic via
-       provided filter argument), parse certain IP header fields, perform AS
-       lookup, and store in database.
+    1. Python utility that collects or captures traffic, parses, and stores
+       into database. Scapy is used for live capture and currenly netflow v5
+       is supported for collecting.
 
     2. Flask web application that reads from the database and creates a couple
        charts in javascript with the help of `chartkick`_.
@@ -36,13 +35,37 @@ and::
 Usage is pretty straightforward. There are two binary files that will be
 installed into your environment.
 
-To start the packet capture and begin storing data into the database::
+To start storing traffic info into database, follow the usage doc for
+``asn_capture``::
 
-    asn_capture [-h | --help | "tcpdump filter"]
+    asn_capture
 
-Note that if no filter is provided "ip" will be used and also a provided filter
-must be enclosed in quotes. Capture should be started before webserver because
-it will initialize the db if it doesn't exist yet.
+    Description: Performs AS lookup for the destination of every packet processed
+                 and stores in database for later viewing in web summary.
+
+                 Traffic can either be gathered via a live capture with custom
+                 filter (pcap) or via netflow by sending flows to the process
+                 (flow).
+
+    Usage:       asn_capture (pcap|flow) [options]
+
+    Options:
+
+        --help                          Show usage
+        -f --filter=<pcap_filter>       PCAP filter syntax. Passed directly to
+                                        scapy. [default: ip]
+        --nflow=<flow_version>          Version of netflow to collect. Currently
+                                        only 'v5' is supported. [default: v5]
+        -h --host=<host>                IP address to bind flow collector on
+                                        [default: 0.0.0.0]
+        -p --port=<port>                Port to listen on for flow collector
+                                        [default: 2303]
+
+
+
+Capture should be started before webserver because it will initialize the db if 
+it doesn't exist yet. If using netflow, once started feel free to start pushing
+flows to it from your router, fw, etc!
 
 To start the webserver to easily look at summary of the data::
 
@@ -52,6 +75,11 @@ That should spawn a local webserver reachable via http://localhost:5000/
 
 Side Notes
 ----------
+
+Only Netflow v5 is supported because I haven't been able to find a v9, sflow,
+or ipfix collector written in Python. Netflow v5 is pretty simple and I was
+able to find a few examples already written to tweak. Who knows, maybe at some
+point I'll fill the gap.
 
 The AS lookup is performed using the pyasn library which uses a view from local
 file to map prefixes to ASN's.
--- asn_report/asn_capture.py
+++ asn_report/asn_capture.py
@@ -1,13 +1,28 @@
 #!/bin/env python2.7
 
 """
-gather_asn.py
+asn_capture
 
-Description: Captures all IP packets (or according to provided scapy filter),
-             parses them, and stores ASN info in SQLite DB for reporting in
-             Flask application.
+Description: Performs AS lookup for the destination of every packet processed
+             and stores in database for later viewing in web summary.
 
-Usage:       gather_asn.py ["filter"]
+             Traffic can either be gathered via a live capture with custom
+             filter (pcap) or via netflow by sending flows to the process
+             (flow).
+
+Usage:       asn_capture (pcap|flow) [options]
+
+Options:
+
+    --help                          Show usage
+    -f --filter=<pcap_filter>       PCAP filter syntax. Passed directly to
+                                    scapy. [default: ip]
+    --nflow=<flow_version>          Version of netflow to collect. Currently
+                                    only 'v5' is supported. [default: v5]
+    -h --host=<host>                IP address to bind flow collector on
+                                    [default: 0.0.0.0]
+    -p --port=<port>                Port to listen on for flow collector
+                                    [default: 2303]
 
 """
 
@@ -15,6 +30,7 @@ import sys
 import pyasn
 import asn_report
 from os import path
+from docopt import docopt
 from sqlalchemy.exc import OperationalError
 from IPy import IP
 from scapy.all import sniff
@@ -25,6 +41,132 @@ asn_db_path = path.abspath(
         path.join(path.dirname(asn_report.__file__), 'resources/ip_to_asn.db'))
 asn_db = pyasn.pyasn(asn_db_path)
 
+
+class IPHeader(object):
+    '''Phony IPHeader class to pair with phony Packet class'''
+    def __init__(self, saddr, daddr):
+        self.src = saddr
+        self.dst = daddr
+
+class Packet(object):
+    '''Class to very minimally imitate scapy's packet structure
+
+    This is to be able to have the same parse_packet function based on the
+    scapy interface regardless of which is used.
+    '''
+    def __init__(self,
+                 saddr,
+                 daddr,
+                 sport,
+                 dport,
+                 protocol):
+        self.saddr = saddr
+        self.daddr = daddr
+        self.sport = sport
+        self.dport = dport
+        self.protocol = protocol
+        ip_header = IPHeader(saddr, daddr)
+        self.headers = {
+                'IP': ip_header
+        }
+    def __getitem__(self, key):
+        return self.headers[key]
+    def summary(self):
+        '''Print packet summary'''
+        keys = {
+                'proto': self.protocol,
+                'saddr': self.saddr,
+                'daddr': self.daddr,
+                'sport': self.sport,
+                'dport': self.dport,
+        }
+        print '{proto} {saddr}:{sport} > {daddr}:{dport}'.format(**keys)
+
+
+def netflow_v5_capture(host='0.0.0.0', port=2303, callback=None):
+    '''Netflow version 5 collector with callback feature for every flow rcv'd
+
+    Most of code for this collector reused from `devicenull`_
+
+    .. _devicenull:
+        http://blog.devicenull.org/2013/09/04/python-netflow-v5-parser.html
+
+    :param host: IP to bind socket to
+    :type host: str
+    :param port: Port to bind to
+    :type port: int
+    :param callback: Callback function. Flow record dict will be passed as only
+                     argument for each received.
+    :type callback: function
+
+    '''
+
+
+    import socket, struct
+    from socket import inet_ntoa
+
+    # Verify ``host`` is a valid IP address
+    try:
+        IP(host)
+    except ValueError:
+        sys.exit('ERROR: Invalid IP address provided')
+
+    SIZE_OF_HEADER = 24
+    SIZE_OF_RECORD = 48
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+
+    while True:
+        buf, addr = sock.recvfrom(1500)
+
+        (version, count) = struct.unpack('!HH',buf[0:4])
+        if version != 5:
+            print "Not NetFlow v5!"
+            continue
+
+        # It's pretty unlikely you'll ever see more then 1000 records in a 
+        # 1500 byte UDP packet
+        if count <= 0 or count >= 1000:
+            print "Invalid count %s" % count
+            continue
+
+        uptime = socket.ntohl(struct.unpack('I',buf[4:8])[0])
+        epochseconds = socket.ntohl(struct.unpack('I',buf[8:12])[0])
+
+        for i in range(0, count):
+            try:
+                base = SIZE_OF_HEADER+(i*SIZE_OF_RECORD)
+
+                data = struct.unpack('!IIIIHH',buf[base+16:base+36])
+
+                nfdata = {}
+                nfdata['saddr'] = inet_ntoa(buf[base+0:base+4])
+                nfdata['daddr'] = inet_ntoa(buf[base+4:base+8])
+                nfdata['pcount'] = data[0]
+                nfdata['bcount'] = data[1]
+                nfdata['stime'] = data[2]
+                nfdata['etime'] = data[3]
+                nfdata['sport'] = data[4]
+                nfdata['dport'] = data[5]
+                nfdata['protocol'] = ord(buf[base+38])
+            except:
+                continue
+
+        packet = Packet(
+                nfdata['saddr'],
+                nfdata['daddr'],
+                nfdata['sport'],
+                nfdata['dport'],
+                nfdata['protocol']
+            )
+        if callback:
+            # If anything is returned, print to stdout
+            returned = callback(packet)
+            if returned:
+                print returned
+        else:
+            packet.summary()
 
 def create_db():
         db.create_all()
@@ -61,9 +203,15 @@ def parse_packet(packet):
     # None as AS currently.
     if IP(dst_ip).iptype() is 'PUBLIC':
         dst_asn, dst_pfx = asn_db.lookup(dst_ip)
-        # Need to implement AS whois in a way that won't get flagged for too
-        # many requests
-        owner = 'NotImplemented'
+
+        # Make sure that None doesn't get returned if db out of date
+        if dst_asn is None:
+            dst_asn = '00000'
+            owner = "IPtoASN Error"
+        else:
+            # Need to implement AS whois in a way that won't get flagged for
+            # too many requests
+            owner = 'NotImplemented'
         args = (dst_asn, owner, dst_ip, dst_pfx)
         add_to_sql(*args)
         return "AS%d owned by %s: %s child of %s" % args
@@ -71,13 +219,29 @@ def parse_packet(packet):
 
 def main():
 
-    try:  # No reason to have a fancy argparser for one argument
-        if sys.argv[1] in ['-h', '--help']:
-            sys.exit(__doc__)
-        scapy_filter = sys.argv[1]
+    args = docopt(__doc__)
+    from pprint import pprint
+    pprint(args)
+
+    # Check whether to packet capture or collect flows
+    if args['pcap']:
+
+        scapy_filter = args['--filter']
         sniff(filter=scapy_filter, prn=parse_packet)
-    except IndexError:
-        sniff(filter="ip", prn=parse_packet)
+
+    elif args['flow']:
+
+        flow_version = args['--nflow']
+        bind_on = args['--host']
+        listen_on = int(args['--port'])
+
+        # Call proper netflow collector
+        if flow_version == 'v5':
+            netflow_v5_capture(host=bind_on,
+                               port=listen_on,
+                               callback=parse_packet)
+        else:
+            sys.exit('Only netflow v5 supported')
 
 
 if __name__ == '__main__':
